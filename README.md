9d7ee31cbfd67ecb9ce2879089171b1c663f6f42
Ameer Sampang Rai <97660344+raijin-asr@users.noreply.github.com>
2025-05-07 12:35:31 +0545

    Day 4: Implement button to pick selected image

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # My-first-react-native-app
 
-My first React Native App developed from docs guide
+My first React Native App using Expo (Guide from Documentation)
 
 # Things i've learned and implemented(day wise)
 
     Day 1: Setup, create first react native app using Expo
     Day 2: Add Navigation, Stack and Tabs
     Day 3: Build a Screen with ImageViewer, Button
+    Day 4: Implement button to pick selected image
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,36 @@
 import { Text, View,  StyleSheet } from 'react-native';
 import ImageViewer from '@/components/ImageViewer';
 import Button from '@/components/Button';
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
 
 const PlaceholderImage = require('@/assets/images/me.jpeg');
 
 export default function Index() {
+
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri); // Set the selected image URI 
+    }else {
+      console.log('No image selected');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage} />
+        <ImageViewer imgSource={PlaceholderImage}  selectedImage={selectedImage}/>
       </View>
       <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" />
+        <Button theme="primary" label="Choose a photo" onPress={pickImage}/>
         <Button label="Use this photo" />
       </View>
      
